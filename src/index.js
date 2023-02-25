1e3d337e9ee3c8c75a62dee880edb0f37e4b9765
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-02-25 17:06:31 +0300

    Modify index.js for tasks

--- src/index.js
+++ src/index.js
@@ -1,86 +1,34 @@
+import Tasks from './modules/Tasks.js';
+
 import './style.css';
 
-const toDoLists = [
-  {
-    description: 'Read all the lesson articles.',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Complete all the projects.',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Do the quiz.',
-    completed: false,
-    index: 2,
-  },
-];
+const newTask = {};
+const taskList = new Tasks();
 
-function Todo(toDoLists) {
-  let isSelected = false;
-  const li = document.createElement('li');
-  const itemTaskElement = document.createElement('div');
-  const checkBox = document.createElement('input');
-  const checkBoxContainer = document.createElement('div');
-  checkBoxContainer.classList.add('checkbox-container');
-  const label = document.createElement('label');
-  const iconMenu = document.createElement('div');
-  const iconDelete = document.createElement('div');
-  iconMenu.classList.add('menu-container');
-  itemTaskElement.classList.add('list-element');
-  iconMenu.innerHTML = '<i class="fas fa-ellipsis-v"></i>';
-  iconDelete.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  checkBox.type = 'checkbox';
-  checkBox.id = `check${toDoLists.index}`;
-  label.htmlFor = `check${toDoLists.index}`;
-  label.innerText = toDoLists.description;
-
-  label.addEventListener('click', () => {
-    isSelected = !isSelected;
-    if (isSelected === true) {
-      label.style.textDecoration = 'line-through';
-    } else {
-      label.style.textDecoration = 'none';
-    }
-  });
-
-  checkBox.addEventListener('click', () => {
-    if (checkBox.checked) {
-      label.style.textDecoration = 'line-through';
-    } else {
-      label.style.textDecoration = 'none';
-    }
-  });
-
-  checkBoxContainer.append(checkBox, label);
-  itemTaskElement.append(checkBoxContainer, iconMenu);
-  li.appendChild(itemTaskElement);
-  return li;
+if (localStorage.TodoDB) {
+  taskList.tasks = JSON.parse(localStorage.getItem('TodoDB'));
 }
 
-function component() {
-  const container = document.createElement('div');
-  const input = document.createElement('input');
-  const ul = document.createElement('ul');
-  const btnClear = document.createElement('button');
-  container.classList.add('list-container');
-  input.placeholder = 'Add to your list...';
-  btnClear.textContent = 'Clear all completed';
-  btnClear.classList.add('clear-btn');
-  btnClear.disabled = true;
+const clearAll = document.querySelector('.clear-btn');
+const root = document.querySelector('.main-container');
+const inputElement = document.querySelector('input');
 
-  toDoLists.map((toDoLists) => {
-    ul.appendChild(Todo(toDoLists));
-    return 'Completed';
-  });
+inputElement.addEventListener('change', () => {
+  newTask.description = inputElement.value;
+  newTask.completed = false;
+  newTask.index = taskList.tasks.length;
+  taskList.addTask(new Tasks(newTask.description, newTask.completed, newTask.index));
+});
 
-  container.append(input, ul, btnClear);
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    inputElement.value = '';
+  }
+});
 
-  return container;
-}
+clearAll.addEventListener('click', () => {
+  // implement clear btn
+});
 
-const mainContainer = document.querySelector('.main-container');
-
-mainContainer.appendChild(component());
+taskList.displayTasks();
+root.append(clearAll);
